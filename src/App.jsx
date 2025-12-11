7793577787ed44fa2005bbc461aa4d6805720d9d
SHASHANK <shashankdj622@gmail.com>
2025-12-11 14:45:07 +0530

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Research from "./components/Research";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div className="font-sans">
+      <Hero />
+      <Projects />
+      <Experience />
+      <Research />
+      <Footer />
+    </div>
+  );
+}
