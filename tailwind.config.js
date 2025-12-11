223d474cac2b7fa952f42b11812ff578586c91ef
SHASHANK <shashankdj622@gmail.com>
2025-12-11 14:44:22 +0530

    Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0b6cff",
+        dark: "#0f172a",
+        light: "#f1f5f9"
+      }
+    }
+  },
+  plugins: []
+};
