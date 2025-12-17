6095c1e49cc98416a11d29c535e558b4297b0195
SHASHANK <shashankdj622@gmail.com>
2025-12-17 14:39:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 
 import Hero from "./components/Hero";
+import ProductVenture from "./components/ProductVenture";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Research from "./components/Research";
 import Footer from "./components/Footer";
-import ProductVenture from "./components/ProductVenture";
 
 export default function App() {
   return (
     <div className="font-sans">
       <Hero />
+      <ProductVenture />
       <Projects />
       <Experience />
       <Research />
       <Footer />
-      <ProductVenture />
     </div>
   );
 }
+
 
 
 
