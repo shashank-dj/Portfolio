fd440b534a638006b21256a375085d65113cf312
SHASHANK <shashankdj622@gmail.com>
2026-03-14 12:29:39 +0100

    Create ResearchPage.jsx

---
+++ src/pages/ResearchPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Research from "../components/Research";
+
+export default function ResearchPage() {
+  return (
+    <section className="bg-dark text-white py-20 px-6">
+
+      <h1 className="text-4xl font-bold text-center text-primary mb-12">
+        Research Publications
+      </h1>
+
+      <Research />
+
+    </section>
+  );
+}
