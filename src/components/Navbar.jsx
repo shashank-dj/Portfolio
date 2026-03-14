0fc6a9eb53fb41874a8769770704f28fef7fa149
SHASHANK <shashankdj622@gmail.com>
2026-03-14 12:20:00 +0100

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-dark text-white flex justify-center gap-8 py-6">
+
+      <Link to="/">Home</Link>
+
+      <Link to="/about">About</Link>
+
+      <Link to="/case-studies">Case Studies</Link>
+
+      <Link to="/ventures">Ventures</Link>
+
+      <Link to="/projects">Projects</Link>
+
+      <Link to="/research">Research</Link>
+
+    </nav>
+  );
+}
