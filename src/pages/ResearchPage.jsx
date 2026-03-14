38941d3243f48a9ed14eb089ea494eaf835cd8d6
SHASHANK <shashankdj622@gmail.com>
2026-03-14 16:16:30 +0100

    Update ResearchPage.jsx

--- src/pages/ResearchPage.jsx
+++ src/pages/ResearchPage.jsx
@@ -5,10 +5,6 @@ export default function ResearchPage() {
   return (
     <section className="bg-dark text-white py-20 px-6">
 
-      <h1 className="text-4xl font-bold text-center text-primary mb-12">
-        Research Publications
-      </h1>
-
       <Research />
 
     </section>
