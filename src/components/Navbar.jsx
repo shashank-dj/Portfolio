9e68222a90ba33d265300588702da7c963db326f
SHASHANK <shashankdj622@gmail.com>
2026-03-14 18:26:56 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <nav className="bg-dark text-white flex justify-center gap-8 py-6">
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
-      // <Link to="/case-studies">Case Studies</Link>
+      {/* <Link to="/case-studies">Case Studies</Link> */}      
       <Link to="/ventures">Ventures</Link>
       <Link to="/projects">Projects</Link>
       <Link to="/research">Research</Link>
