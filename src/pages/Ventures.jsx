651b34dffdd515b98cc2c4fafbd1e6e14cdb58da
SHASHANK <shashankdj622@gmail.com>
2026-03-14 12:28:46 +0100

    Create Ventures.jsx

---
+++ src/pages/Ventures.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ProductVenture from "../components/ProductVenture";
+
+export default function Ventures() {
+  return (
+    <section className="bg-dark text-white py-20 px-6">
+
+      <h1 className="text-4xl font-bold text-center text-primary mb-12">
+        Product Ventures
+      </h1>
+
+      <ProductVenture />
+
+    </section>
+  );
+}
