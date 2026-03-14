1a795c7e08c74df43522b88f2b61ac3f30e3a392
SHASHANK <shashankdj622@gmail.com>
2026-03-14 13:39:36 +0100

    Update Ventures.jsx

--- src/pages/Ventures.jsx
+++ src/pages/Ventures.jsx
@@ -4,11 +4,6 @@ import ProductVenture from "../components/ProductVenture";
 export default function Ventures() {
   return (
     <section className="bg-dark text-white py-20 px-6">
-
-      <h1 className="text-4xl font-bold text-center text-primary mb-12">
-        Product Ventures
-      </h1>
-
       <ProductVenture />
 
     </section>
