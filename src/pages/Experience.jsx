6c2878ff797381910f5ea865ad5942fc8efca67f
SHASHANK <shashankdj622@gmail.com>
2026-03-14 17:12:34 +0100

    Create Experience.jsx

---
+++ src/pages/Experience.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProductVenture from "../components/Experience";
+
+export default function Ventures() {
+  return (
+    <section className="bg-dark text-white py-20 px-6">
+      <Experience.jsx />
+
+    </section>
+  );
+}
