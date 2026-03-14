2aef37d3ddc31bd7cca0054e4094a84644a20d7f
SHASHANK <shashankdj622@gmail.com>
2026-03-14 17:37:07 +0100

    Update ExperiencePage.jsx

--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -4,6 +4,9 @@ import Experience from "../components/Experience";
 export default function Ventures() {
   return (
     <section className="bg-dark text-white py-20 px-6">
+      <h1 className="text-4xl font-bold text-center text-primary mb-12">
+        Experience
+      </h1>
       <Experience />
 
     </section>
