7584a112bf59810c34fa7e112eef04699856e136
SHASHANK <shashankdj622@gmail.com>
2026-03-14 13:41:03 +0100

    Update ProjectsPage.jsx

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -6,7 +6,7 @@ export default function ProjectsPage() {
     <section className="bg-dark text-white py-20 px-6">
 
       <h1 className="text-4xl font-bold text-center text-primary mb-12">
-        Technical Projects
+        Projects
       </h1>
 
       <Projects />
