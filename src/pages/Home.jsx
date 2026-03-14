c3783358cdcca2719dbcd4f92400bdfca09e6a8e
SHASHANK <shashankdj622@gmail.com>
2026-03-14 13:36:05 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
-import ProductVenture from "../components/ProductVenture";
-import Projects from "../components/Projects";
-import Research from "../components/Research";
-import Experience from "../components/Experience";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <ProductVenture />
-      <Projects />
-      <Research />
-      <Experience />
-    </>
-  );
+return ( <Hero />
+);
 }
