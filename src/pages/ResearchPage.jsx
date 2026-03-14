88cc9ddca6bc1e8c1eb8116767b8be90661947e8
SHASHANK <shashankdj622@gmail.com>
2026-03-14 16:19:31 +0100

    Update ResearchPage.jsx

--- src/pages/ResearchPage.jsx
+++ src/pages/ResearchPage.jsx
@@ -4,7 +4,9 @@ import Research from "../components/Research";
 export default function ResearchPage() {
   return (
     <section className="bg-dark text-white py-20 px-6">
-
+      <h1 className="text-4xl font-bold text-center text-primary mb-12">
+        Projects
+      </h1>
       <Research />
 
     </section>
