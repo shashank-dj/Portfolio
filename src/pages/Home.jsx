450f2e309e87654edfbb6ba829b88596017b3eb9
SHASHANK <shashankdj622@gmail.com>
2026-03-14 12:18:49 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,18 @@
 
+import Hero from "../components/Hero";
+import ProductVenture from "../components/ProductVenture";
+import Projects from "../components/Projects";
+import Research from "../components/Research";
+import Experience from "../components/Experience";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <ProductVenture />
+      <Projects />
+      <Research />
+      <Experience />
+    </>
+  );
+}
