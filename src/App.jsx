646c25aceadf74f60d48164f2d0176eccd1559d7
SHASHANK <shashankdj622@gmail.com>
2026-03-14 12:51:59 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
-import About from "./pages/About";
-import CaseStudies from "./pages/CaseStudies";
-import Ventures from "./pages/Ventures";
-import ProjectsPage from "./pages/ProjectsPage";
-import ResearchPage from "./pages/ResearchPage";
-
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-
-      <Navbar />
-
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/case-studies" element={<CaseStudies />} />
-        <Route path="/ventures" element={<Ventures />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/research" element={<ResearchPage />} />
       </Routes>
-
-      <Footer />
-
     </BrowserRouter>
   );
 }
