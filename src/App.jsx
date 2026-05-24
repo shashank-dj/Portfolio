21ef3ab32fe5f48acb4ef062b90319fca086c7a5
SHASHANK <shashankdj622@gmail.com>
2026-05-24 12:21:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
 import React, { useEffect } from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  useLocation,
-} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Cursor from './components/Cursor';
+import Navbar         from './components/Navbar';
+import Footer         from './components/Footer';
+import Cursor         from './components/Cursor';
 
-import Home from './pages/Home';
-import About from './pages/About';
-import CaseStudies from './pages/CaseStudies';
-import Ventures from './pages/Ventures';
-import ProjectsPage from './pages/ProjectsPage';
-import ResearchPage from './pages/ResearchPage';
+import Home           from './pages/Home';
+import About          from './pages/About';
+import CaseStudies    from './pages/CaseStudies';
+import Ventures       from './pages/Ventures';
+import ProjectsPage   from './pages/ProjectsPage';
+import ResearchPage   from './pages/ResearchPage';
 import ExperiencePage from './pages/ExperiencePage';
-
 import ZomatoCaseStudy from './pages/caseStudies/Zomato';
 import CargillCaseStudy from './pages/caseStudies/cargill';
 import CursorPMCaseStudy from './pages/caseStudies/CursorPM';
-import JulesPM from './pages/caseStudies/Jules';
+import JulesCaseStudy from './pages/caseStudies/Jules';
+import NShiftCaseStudy from './pages/caseStudies/nShift';
 
 /* Scroll to top on every navigation */
 function ScrollReset() {
   const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'instant',
-    });
-  }, [pathname]);
-
+  useEffect(() => { window.scrollTo({ top: 0, behavior: 'instant' }); }, [pathname]);
   return null;
 }
 
@@ -41,71 +29,23 @@ function AppInner() {
   return (
     <div className="min-h-screen flex flex-col bg-void text-zinc-100 font-jakarta">
       <Cursor />
-
       <ScrollReset />
-
       <Navbar />
 
       <main className="flex-grow">
         <Routes>
-
-          <Route
-            path="/"
-            element={<Home />}
-          />
-
-          <Route
-            path="/about"
-            element={<About />}
-          />
-
-          <Route
-            path="/case-studies"
-            element={<CaseStudies />}
-          />
-
-          <Route
-            path="/ventures"
-            element={<Ventures />}
-          />
-
-          <Route
-            path="/projects"
-            element={<ProjectsPage />}
-          />
-
-          <Route
-            path="/research"
-            element={<ResearchPage />}
-          />
-
-          <Route
-            path="/experience"
-            element={<ExperiencePage />}
-          />
-
-          {/* Case Studies */}
-
-          <Route
-            path="/case-studies/zomato"
-            element={<ZomatoCaseStudy />}
-          />
-
-          <Route
-            path="/case-studies/cargill"
-            element={<CargillCaseStudy />}
-          />
-
-          <Route
-            path="/case-studies/cursor-pm"
-            element={<CursorPMCaseStudy />}
-          />
-
-          <Route
-            path="/case-studies/jules"
-            element={<JulesPM />}
-          />
-
+          <Route path="/"                    element={<Home />} />
+          <Route path="/about"               element={<About />} />
+          <Route path="/case-studies"        element={<CaseStudies />} />
+          <Route path="/case-studies/zomato" element={<ZomatoCaseStudy />} />
+          <Route path="/ventures"            element={<Ventures />} />
+          <Route path="/projects"            element={<ProjectsPage />} />
+          <Route path="/research"            element={<ResearchPage />} />
+          <Route path="/experience"          element={<ExperiencePage />} />
+          <Route path="/case-studies/cargill" element={<CargillCaseStudy />} />
+          <Route path="/case-studies/cursor-pm" element={<CursorPMCaseStudy />} />
+          <Route path="/case-studies/jules" element={<JulesCaseStudy />} />     
+          <Route path="/case-studies/nShift" element={<NShiftCaseStudy />} />     
         </Routes>
       </main>
 
